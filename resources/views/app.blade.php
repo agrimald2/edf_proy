ec4f4aaf281b61a8f20b9b66e071f480ea1c1da6
agrimal2 <agrimaldopci18@gmail.com>
2024-03-15 17:07:05 -0500

    buildñ

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <meta name="description" content="Proyecto EDF - Salvador Palacios">
+        <meta name="author" content="Salvador Palacios">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
