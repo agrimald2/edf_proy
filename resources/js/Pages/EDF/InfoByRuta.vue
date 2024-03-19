203da2bba53e1e52ac104d284670885f17f3f627
agrimal2 <agrimaldopci18@gmail.com>
2024-03-19 13:21:06 -0500

    Fixed

--- resources/js/Pages/EDF/InfoByRuta.vue
+++ resources/js/Pages/EDF/InfoByRuta.vue
@@ -16,15 +16,16 @@ const selectedDay = ref('');
 
 // Function to get the current day of the week
 const getCurrentDayOfWeek = () => {
-  const days = ['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO'];
-  return days[new Date().getDay()];
+    const days = ['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO'];
+    return days[new Date().getDay()];
 };
 
 onMounted(() => {
-  selectedDay.value = getCurrentDayOfWeek();
+    selectedDay.value = getCurrentDayOfWeek();
 });
 
 const filteredClients = computed(() => {
+    let clientsMap = new Map(); // Mapa para rastrear los COD_CLIENTE únicos
     let clients = props.clients.filter(client => {
         const matchesCOD_CLIENTE = client.COD_CLIENTE.toLowerCase().includes(searchQuery.value.toLowerCase());
         const matchesCLIENTE = client.CLIENTE.toLowerCase().includes(searchQuery.value.toLowerCase());
@@ -37,7 +38,13 @@ const filteredClients = computed(() => {
         } else if (selectedFilter.value === 'negociados' && client.NEGOCIADO === 'NEGOCIADO') {
             matchesFilter = true;
         }
-        return (matchesCOD_CLIENTE || matchesCLIENTE) && matchesFilter && matchesDay;
+        // Verificar si el cliente ya ha sido agregado según su COD_CLIENTE
+        let notAddedYet = !clientsMap.has(client.COD_CLIENTE);
+        if (notAddedYet && (matchesCOD_CLIENTE || matchesCLIENTE) && matchesFilter && matchesDay) {
+            clientsMap.set(client.COD_CLIENTE, true); // Marcar el COD_CLIENTE como agregado
+            return true;
+        }
+        return false;
     });
 
     // Prioritize clients with "PENDIENTE" status
@@ -52,11 +59,27 @@ const filteredClients = computed(() => {
 });
 
 const filteredNegociados = computed(() => {
-    return props.clients.filter(client => client.NEGOCIADO === 'NEGOCIADO' && (client.FREC_VISITA.includes(selectedDay.value) || selectedDay.value === '')).length;
+    let negociadosMap = new Map(); // Mapa para rastrear los COD_CLIENTE de los negociados únicos
+    let count = 0;
+    props.clients.forEach(client => {
+        if (client.NEGOCIADO === 'NEGOCIADO' && (client.FREC_VISITA.includes(selectedDay.value) || selectedDay.value === '') && !negociadosMap.has(client.COD_CLIENTE)) {
+            negociadosMap.set(client.COD_CLIENTE, true); // Marcar el COD_CLIENTE como contado
+            count++;
+        }
+    });
+    return count;
 });
 
 const filteredNoNegociados = computed(() => {
-    return props.clients.filter(client => client.NEGOCIADO === 'PENDIENTE' && (client.FREC_VISITA.includes(selectedDay.value) || selectedDay.value === '')).length;
+    let noNegociadosMap = new Map(); // Mapa para rastrear los COD_CLIENTE de los no negociados únicos
+    let count = 0;
+    props.clients.forEach(client => {
+        if (client.NEGOCIADO === 'PENDIENTE' && (client.FREC_VISITA.includes(selectedDay.value) || selectedDay.value === '') && !noNegociadosMap.has(client.COD_CLIENTE)) {
+            noNegociadosMap.set(client.COD_CLIENTE, true); // Marcar el COD_CLIENTE como contado
+            count++;
+        }
+    });
+    return count;
 });
 </script>
 
@@ -67,7 +90,8 @@ const filteredNoNegociados = computed(() => {
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                     Hola, {{ props.gv.split(' ').slice(1).join(' ') }}!
                 </h2>
-                <select v-model="selectedDay" class="form-select block w-1/4 mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <select v-model="selectedDay"
+                    class="form-select block w-1/4 mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                     <option value="">Seleccione una frecuencia</option>
                     <option value="LUNES">LUNES</option>
                     <option value="MARTES">MARTES</option>
@@ -109,8 +133,7 @@ const filteredNoNegociados = computed(() => {
                     <div class="pt-2 relative mx-auto text-gray-600 w-full">
                         <input
                             class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-full"
-                            type="search" name="search" placeholder="Buscar..."
-                            v-model="searchQuery">
+                            type="search" name="search" placeholder="Buscar..." v-model="searchQuery">
                     </div>
                     <div class="flex gap-2 my-4">
                         <button class="px-4 py-2 text-white rounded-full"
@@ -127,11 +150,15 @@ const filteredNoNegociados = computed(() => {
 
                 <div class="p-2 overflow-hidden shadow-xl sm:rounded-lg">
                     <div class="flex flex-col gap-4">
-                        <div class="bg-white shadow-md rounded-lg overflow-hidden flex" v-for="client in filteredClients">
+                        <div class="bg-white shadow-md rounded-lg overflow-hidden flex"
+                            v-for="client in filteredClients">
                             <div class="flex-1 border-r border-gray-200">
                                 <div class="p-4">
-                                    <div class="text-sm font-semibold"><i class="fa-solid fa-user"></i> {{ client.COD_CLIENTE }} - {{ client.CLIENTE }}</div>
-                                    <div class="text-sm"><i class="fa-solid fa-location-dot"></i> {{ client.DIRECCIÓN }}</div>
+                                    <div class="text-sm font-semibold"><i class="fa-solid fa-user"></i> {{
+        client.COD_CLIENTE }}
+                                        - {{ client.CLIENTE }}</div>
+                                    <div class="text-sm"><i class="fa-solid fa-location-dot"></i> {{ client.DIRECCIÓN }}
+                                    </div>
                                     <div class="mt-2">
                                         <span v-if="client.N_EDF == 0"
                                             class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
@@ -148,9 +175,12 @@ const filteredNoNegociados = computed(() => {
                                 <div class="p-4 text-left">
                                     <div class="text-sm font-medium text-gray-500">Puedes negociar</div>
                                     <div class="flex items-center mt-2">
-                                        <div class="text-sm font-semibold">EDF | {{ client.PUERTAS_A_NEGOCIAR }} Pts | {{ client.CONDICION }}</div>
+                                        <div class="text-sm font-semibold">EDF | {{ client.PUERTAS_A_NEGOCIAR }} Pts |
+                                            {{
+        client.CONDICION }}</div>
                                     </div>
-                                    <a :href="`https://wa.me/?text=Quiero%20negociar%20EDF%20para%20el%20cliente%20${client.COD_CLIENTE}`" target="_blank" class="ml-auto bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-md">
+                                    <a :href="`https://wa.link/ibba8o`" target="_blank"
+                                        class="ml-auto bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-md">
                                         <i class="fa-brands fa-whatsapp"></i>
                                         Negociar EDF
                                     </a>
@@ -158,11 +188,14 @@ const filteredNoNegociados = computed(() => {
                             </div>
                             <div v-else class="flex-1 text-left">
                                 <div class="p-4 text-left">
-                                    <div class="text-sm font-medium text-gray-500"><i class="fa-solid fa-check-to-slot mr-2"></i>Haz Negociado</div>
+                                    <div class="text-sm font-medium text-gray-500"><i
+                                            class="fa-solid fa-check-to-slot mr-2"></i>Haz Negociado</div>
                                     <div class="flex items-center mt-2">
-                                        <div class="text-sm font-semibold">EDF | {{ client.PUERTAS_A_NEGOCIAR }} Pts | {{client.CONDICION}}</div>
+                                        <div class="text-sm font-semibold">EDF | {{ client.PUERTAS_A_NEGOCIAR }} Pts |
+                                            {{client.CONDICION}}</div>
                                     </div>
-                                    <button class="ml-auto bg-green-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-md">
+                                    <button
+                                        class="ml-auto bg-green-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-md">
                                         <i class="fa-solid fa-shuffle"></i>
                                     </button>
                                 </div>
