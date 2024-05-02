a6bb1489f87546ab1b2a1f85f01d2fdc7ad3f562
agrimal2 <agrimaldopci18@gmail.com>
2024-05-02 13:56:17 -0500

    build

--- resources/js/Pages/EDF/InfoByRuta.vue
+++ resources/js/Pages/EDF/InfoByRuta.vue
@@ -243,7 +243,7 @@ const filteredNoNegociados = computed(() => {
                                     <div class="text-xs text-gray-700 mt-1" v-if="client.FECHA_PROGRAMACION">
                                         <strong>
                                             F.E: {{ client.FECHA_PROGRAMACION }}
-                                            <span v-if="new Date(client.FECHA_PROGRAMACION) < new Date()" class="text-red-500">!</span>
+                                            <span v-if="new Date(client.FECHA_PROGRAMACION) < new Date()" class="text-red-500 text-lg">!</span>
                                         </strong>
                                     </div>
                                 </div>
