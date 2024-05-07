802b9ff11fadd3da542111840198f69887df8fd1
agrimal2 <agrimaldopci18@gmail.com>
2024-05-07 02:43:17 -0500

    Build Hotjar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,6 +20,19 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
         <script src="https://kit.fontawesome.com/2780e63ff4.js" crossorigin="anonymous"></script>
+    
+        <!-- Hotjar Tracking Code for https://nedfac.com/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:4938418,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+    
     </head>
     <body class="font-sans antialiased">
         @inertia
