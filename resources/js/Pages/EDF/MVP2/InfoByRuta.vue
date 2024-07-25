20016ec6da336d6c33e9da98c3cf76351f77c667
agrimal2 <agrimaldopci18@gmail.com>
2024-07-25 19:41:43 -0300

    a

--- resources/js/Pages/EDF/MVP2/InfoByRuta.vue
+++ resources/js/Pages/EDF/MVP2/InfoByRuta.vue
@@ -257,8 +257,8 @@ const todaysDate = new Date().toLocaleDateString('es-ES', {
                                     <div class="text-sm font-medium text-black font-semibold">
                                         <i class="fa-solid fa-circle-check mr-2"></i>Has Negociado
                                         <ul class="list-disc pl-3">
-                                            <li style="font-size:10px" class="text-xs mt-1">EDF - {{ client.PUERTAS_A_NEGOCIAR }} Puertas - {{ client.CONDICION.charAt(0).toUpperCase() + client.CONDICION.slice(1).toLowerCase() }}</li>
-                                            <li  v-if="client.PUERTAS_A_NEGOCIAR_2" style="font-size:10px" class="mb-2 text-xs mt-1">EDF - {{ client.PUERTAS_A_NEGOCIAR_2 }} Puertas - {{ client.CONDICION.charAt(0).toUpperCase() + client.CONDICION_2.slice(1).toLowerCase() }}</li>
+                                            <li v-if="client.PUERTAS_A_NEGOCIAR != 0" style="font-size:10px" class="text-xs mt-1">EDF - {{ client.PUERTAS_A_NEGOCIAR }} Puertas - Repotenciado</li>
+                                            <li v-if="client.PUERTAS_A_NEGOCIAR_2 && client.PUERTAS_A_NEGOCIAR_2 != 0" style="font-size:10px" class="mb-2 text-xs mt-1">EDF - {{ client.PUERTAS_A_NEGOCIAR_2 }} Puertas - Nuevo</li>
                                         </ul>
                                     </div>
                                     <ol class="flex items-center w-full mt-4">
