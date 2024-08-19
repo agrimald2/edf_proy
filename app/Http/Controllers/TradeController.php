c37e2da3c5cfaa7643d194ceb1c8c063e02ac20a
agrimald2 <agrimaldopci18@gmail.com>
2024-08-19 16:37:32 -0300

    Update TradeController.php

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -73,7 +73,7 @@ class TradeController extends Controller
 
             $permuta = Permuta::find($row[0]);
             if ($permuta) {
-                Log::debug($permuta);
+                Log::debug($row);
                 if ($row[10] === 'SI') {
                     $permuta->trade_status = 'Approved';
                     $permuta->trade_approved_at = now();
