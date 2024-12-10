dd2cc1ba14848a57719ea680cf1511bf8daaa28f
agrimald2 <agrimaldopci18@gmail.com>
2024-12-10 18:20:57 -0300

    Debug

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -95,6 +95,7 @@ class MainController extends Controller
             $fecha_negociado = $client->FECHA_NEGOCIADO;
 
             if ($fecha_negociado) {
+                Log::debug($fecha_negociado);
                 $fecha_negociado_date = \Carbon\Carbon::createFromFormat('Y-m-d', $fecha_negociado);
                 $fecha_actual = \Carbon\Carbon::now();
 
